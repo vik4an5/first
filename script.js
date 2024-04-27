6e8abfd8dcffe16f90999ff5d8f981ccab58b18d
vik4an5 <168275876+vik4an5@users.noreply.github.com>
2024-04-27 17:20:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const target = document.getElementById('target');
 let yes = document.getElementById('yes');
 
 yes.addEventListener('click', () => {
-    alert("I love you too jaan mera bab u😘😘😘");
+    alert("I love you too Cutie pie😘😘😘");
 })
 
 function moveTarget(){
